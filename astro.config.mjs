cfe8cbc6a83884e97b280e2df88f9c0843370931
Tim Lawrence <tlawren@gmail.com>
2024-10-17 16:35:58 -0400

    updating config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tlawrence36.github.io',
-    base: 'my-feeds',
+    base: 'rss-feeds',
 });

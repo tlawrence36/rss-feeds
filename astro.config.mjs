5efe698c820dc7b91862141d58c4e7a201d8a0cd
Tim Lawrence <home@Tims-MacBook-Pro.local>
2024-10-17 16:28:18 -0400

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://tlawrence36.github.io',
+    base: 'my-feeds',
+});

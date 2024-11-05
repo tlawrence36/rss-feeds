37d3b1c3f35a6941fa706541b937fb0e8c731818
Tim Lawrence <tlawren@gmail.com>
2024-11-05 09:11:12 -0500

    filtering articles to last seven days

--- feed/get_feeds.py
+++ feed/get_feeds.py
@@ -2,10 +2,19 @@ import feedparser
 import requests
 from bs4 import BeautifulSoup
 import json
+import datetime
 
 feed_list = [] # list of feeds
 parsed_articles = [] # articles from feeds
 content = [] # JSON of all content
+
+def filter_period(period, date_str):
+    month_map = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+    parts = date_str.split()
+    article_date = datetime.date(int(parts[3]), month_map[parts[2]], int(parts[1]))
+    today = datetime.date.today()
+    week_ago = today - datetime.timedelta(days=period)
+    return article_date > week_ago
 
 def fetch_feeds_list(): # use filename param?
     print('get list of feeds to use')
@@ -20,8 +29,8 @@ def fetch_feeds_list(): # use filename param?
 def parse_rss_feeds():
     print('parse feeds to get articles')
     for feed in feed_list:
-        theFeed = feedparser.parse(feed[2])
-        for entry in theFeed.entries:
+        the_feed = feedparser.parse(feed[2])
+        for entry in the_feed.entries:
             item = {
                 "source": feed[0],
                 "category": feed[1],
@@ -50,31 +59,34 @@ def scrape_articles():
     print('scrape articles, save to JSON for astro')
     article_id = 1
     for article in parsed_articles:
-        if article['link'].find('video') == -1:
-            # print(article['link'])
-            response = requests.get(article['link'])#
-            soup = BeautifulSoup(response.content, 'html.parser')
-            container = get_container(soup, article['source'])
+        # only include articles from the past 7 days
+        if filter_period(7, article['published']):
+            if article['link'].find('video') == -1:
+                # print(article['link'])
+                response = requests.get(article['link'])#
+                soup = BeautifulSoup(response.content, 'html.parser')
+                container = get_container(soup, article['source'])
 
-            if container is not None:
-                item = {
-                    "id": article_id,
-                    "source": article['source'],
-                    "category": article['category'],
-                    "title": article['title'],
-                    "published": article['published'],
-                    "summary": article['summary'],
-                }
+                if container is not None:
+                    item = {
+                        "id": article_id,
+                        "source": article['source'],
+                        "category": article['category'],
+                        "title": article['title'],
+                        "published": article['published'],
+                        "summary": article['summary'],
+                    }
 
-                lines = container.find_all(["p", "h2", "ul", "li"])
-                story = []
-                for line in lines:
-                    if line.text != '':
-                        story.append(line.text.strip())
-                item['story'] = story
-                content.append(item)
-                article_id += 1
+                    lines = container.find_all(["p", "h2", "ul", "li"])
+                    story = []
+                    for line in lines:
+                        if line.text != '':
+                            story.append(line.text.strip())
+                    item['story'] = story
+                    content.append(item)
+                    article_id += 1
 
+    # print(content)
     json_content = json.dumps(content, indent=2)
     with open("feed.json", "w", encoding="utf-8") as f:
         f.write(json_content)
